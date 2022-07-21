b01ce441ba451a5cb69c67c31aef8dcb6d6a278d
Arvind Chauhan <arvindgta520@gmail.com>
2022-07-21 16:12:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import "./App.css";
 import Search from "./Components/Search";
 import MovieList from "./Components/MovieList";
+import { useContext } from "react";
+import { GlobalContext } from "./GlobalContext";
 
 function App() {
+  const { movieList } = useContext(GlobalContext);
   return (
-    <div className="App">
+    <div
+      style={{ backgroundColor: movieList.length > 0 ? "whitesmoke" : null }}
+      className="App"
+    >
       <Search />
       <MovieList />
     </div>
